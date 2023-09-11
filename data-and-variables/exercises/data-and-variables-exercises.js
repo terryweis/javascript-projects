6b69b576083f82ca0fb0652df34390eeff349b80
terryweis <weist2088@gmail.com>
2023-09-11 08:58:19 -0500

    <Timeline to Mars and the Moon>

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,75 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+
+// Name of the space shuttle//
+
+let shuttle = "Determination";
+
+//Shuttle Speed (MPH)//
+
+let speed = 17500;
+
+// Distance to Marks (KM)//
+
+let distance = 225000000;
+
+//Distance to Moon (KM)//
+
+let distance2 = 384400;
+
+// Miles per Kilometer//
+
+let mpk = 0.621;
+
+console.log(typeof shuttle);
+
+console.log(typeof speed);
+
+console.log(typeof distance);
+
+console.log(typeof distance2);
+
+console.log(typeof mpk);
+
+
+// Miles and to Mars//
+
+let marsMiles = distance * mpk;
+
+console.log(marsMiles, "Miles to Mars");
+
+// Hours to Mars//
+
+let hoursToMars = marsMiles / speed;
+
+console.log(hoursToMars, "Hours to Mars");
+
+// Days to get to Mars//
+
+let daysToMars = hoursToMars / 24;
+
+console.log(daysToMars,"days to Mars");
+
+console.log(shuttle +"will take" + daysToMars + "days to reach Mars.");
+
+// Miles to the Moon //
+
+let moonMiles = distance2 * mpk;
+
+console.log(moonMiles, "Miles to the Moon");
+
+// Hours to Moon //
+
+let hourstoMoon = moonMiles / speed;
+
+console.log(hourstoMoon, "hours to The Moon");
+
+// Days to get to Mars //
+
+let daystoMoon = hourstoMoon / 24;
+
+console.log(daystoMoon, "days to get to the Moon");
+
+console.log(shuttle + "will take" + daystoMoon + "days to reach the Moon");
--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -2,11 +2,19 @@
 
    // console.log("This does not print.");
 
+   // seperating comments can make them eaiser to read
+
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
    /* Here is how
    to have
    multi-line
    comments. */
+
+   /* Multi-line
+   comments are useful for larger
+   amount of information but remember to close
+   your comments or everyhitng after is included as a comment*/
+
 
    console.log("Comments make your code more readable by others.");
