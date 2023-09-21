54e963deb42c2720196b79830558ea1e468077fb
terryweis <weist2088@gmail.com>
2023-09-21 11:06:21 -0500

    Loop Examples

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,9 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
+
+
+for (let i = 2; i > -51; i = i - 2) {
    console.log(i);
  }
+
+ 
--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,11 @@
 // create an array variable containing the names
 
+let names = "Terry, Ander, Tipsy, Parker, Katara";
+let arrayNames = [];
+arrayNames = names.split(", ");
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < arrayNames.length; i++){
+console.log(arrayNames[i]);
+}
