8c8167402b3d4308739228b05d5a2b7f2c98a75d
terryweis <weist2088@gmail.com>
2023-09-21 11:03:04 -0500

    For and While Loops

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -5,7 +5,37 @@
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
 
+    /*
+//a.
 
+for (let i = 0;i < 21; i++){
+console.log(i);
+
+}
+
+//b.
+
+for (let i = 3; i < 30; i = i + 2){
+  console.log(i);
+}
+
+//c.
+
+for (let i = 12; i > -14; i -= 2){
+  console.log(i);
+}
+
+//d. 
+
+for (let i = 50; i > 20; i -= 1){
+
+  if (i % 3 > 0){
+
+  }else{
+    console.log(i);
+  }
+}
+/*
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
@@ -15,10 +45,41 @@ Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+let word = "LaunchCode";
+let array = [2, 3, 13, 18, -5, 38, -10, -11, 0, 104]
 
+//a.
 
+for (let i = 0; i < array.length; i = i + 1){
+  console.log(array[i]);
+}
 
+//b.
+let reversed = "";
+for (let i = 0; i < word.length; i++){
+ reversed = word[i] + reversed;
+}
+console.log(reversed);
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  //a.
+let evens = [];
+let odds = [];
+
+for (let i = 0; i < array.length; i++){
+
+  if (array[i] % 2 === 0){
+evens.push(array[i]);
+  } else if (array[i] % 2 !== 0){
+  odds.push(array[i]);
+  }
+
+
+
+}
+
+console.log(evens);
+console.log(odds);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,53 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+const input = require('readline-sync');
 
-
-
-
+let  startingFuel;
+let astronautsOnBoard; 
+let altitude;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+startingFuel = input.question("Please enter a starting fuel level between 5000 - 30000.");
+startingFuel = Number(startingFuel);
+  
+while (startingFuel > 30000 || startingFuel <= 5000 || isNaN(startingFuel)){
+    startingFuel = input.question("Please enter a starting fuel level between 5000 - 30000.")
+      startingFuel = Number(startingFuel);
+    
+}
 
+console.log(`Starting Fuel is set at ${startingFuel}`);
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
-  
+astronautsOnBoard = input.question("How many astronauts should we send (to a max of 7)?");
+astronautsOnBoard = Number(astronautsOnBoard)
+
+while (astronautsOnBoard <= 0 || astronautsOnBoard > 7){
+  astronautsOnBoard = input.question("How many astronauts should we send (to a max of 7)?");
+astronautsOnBoard = Number(astronautsOnBoard)
+
+}
+console.log(`Awesome! We will prep our team of ${astronautsOnBoard}. Lets see if we can make it into orbit!`);  
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+altitude = 0
+while (startingFuel >= (astronautsOnBoard * 100)){
+  altitude = (altitude + 50);
+  startingFuel = startingFuel - (astronautsOnBoard * 100);
 
-
+}
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+if (altitude >= 2000){
+  console.log(`The shuttle gained an altitude of ${altitude}km. Orbit achieved!`);
+ } else{
+  console.log(`The shuttle gained an alititde of ${altitude}. Failed to reach orbit.`);
+ }
