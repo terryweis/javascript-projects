e486bd76d31dff1924d83653297ee1e890121211
terryweis <weist2088@gmail.com>
2023-10-29 18:22:05 -0500

    Doms and events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', function() {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', function(event){
+        event.target.style.color = 'red';
+    });
+    missionAbort.addEventListener('mouseout', function(event){
+        event.target.style.color = "";
+    });
+    missionAbort.addEventListener('click', function(event){
+       let response = window.confirm("Are you sure you want to abort the mission?")
+       console.log(response)
+       if (response === true){
+        paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.'
+       }
+    })
     
 }
 
