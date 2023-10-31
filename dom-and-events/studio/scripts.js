96f489437e0f0dd40d72a1dc50089e880098eaac
terryweis <weist2088@gmail.com>
2023-10-30 19:29:01 -0500

    DOM

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,26 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init(){
+
+    const takeOff = document.getElementById("takeoff");
+    const flightStatus = doucment.getElementById("flightStatus")
+    const landing = document.getElementById("landing");
+    
+takeOff.addEventListener('click', function(){
+    alert('hello')
+//     if (confirm('Confirm the shuttle is ready for takeoff.')){
+
+        
+//     }
+// })
+
+
+
+
+
+
+
+})}
+
+
+window.addEventListener('load',init);
